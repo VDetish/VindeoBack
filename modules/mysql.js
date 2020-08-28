a13eba1959b205f0740809a3d5386eaaae4dd0fe
Vladislav Donets <d@qw.su>
2020-08-29 02:53:34 +0300

    mysql

---
+++ modules/mysql.js
@@ -0,0 +1,129 @@
+import mysql from 'mysql2'
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'tool46',
+  database: 'toolmi',
+})
+
+// // Add user
+// exports.addUser = (id, callback) => {
+//   connection.query(
+//     'INSERT INTO `users` SET ?',
+//     {
+//       firstName: 'test',
+//       lastName: 'test',
+//       vID: 666,
+//       canWrite: true,
+//       canGetAudio: false,
+//     },
+//     callback
+//   )
+// }
+
+// // Add media
+// exports.addMedia = (media, callback) => {
+//   connection.query(
+//     'insert ignore into instagram_media (mediaID, userName, likeCount, commentCount, caption, webLink, image, lat, lng, city, place, address, takenAt, added) values ?',
+//     [media],
+//     callback
+//   )
+// }
+
+// // Add to queue
+// exports.addToQueue = (queue, callback) => {
+//   connection.query(
+//     'insert ignore into instagram_queue (type, value, added) values ?',
+//     [queue],
+//     callback
+//   )
+// }
+
+// // Get queue
+// exports.getQueue = (callback) => {
+//   connection.query(
+//     'select type, value from instagram_queue WHERE ? order by id LIMIT 1',
+//     { isCompleted: 0 },
+//     callback
+//   )
+// }
+
+// // Update queue
+// exports.updateQueue = (value, callback) => {
+//   connection.query(
+//     'update instagram_queue SET isCompleted = ?, completedAt = ? WHERE value = ?',
+//     [1, new Date(), value],
+//     callback
+//   )
+// }
+
+// Get user
+export function getUser(id, callback) {
+  try {
+    connection.query('SELECT * FROM `users` WHERE ?', { id }, callback)
+  } catch (e) {
+    console.log(e)
+    callback(e)
+  }
+}
+
+// Create user
+export function createUser(
+  { lastName, familyName, birthDate, sex, orientation, location, phone, email },
+  callback
+) {
+  try {
+    connection.query(
+      'INSERT INTO `users` SET ?',
+      {
+        lastName,
+        familyName,
+        birthDate,
+        sex,
+        orientation,
+        location,
+        phone,
+        email,
+      },
+      callback
+    )
+  } catch (e) {
+    console.log(e)
+    callback(e)
+  }
+}
+
+// // Update connection
+// exports.userIsGet = (id, callback) => {
+//   connection.query(
+//     'UPDATE connections SET isGet = ? WHERE value = ?',
+//     [1, id],
+//     callback
+//   )
+// }
+
+// // Get connection
+// exports.getConnection = (callback) => {
+//   connection.query(
+//     'SELECT value FROM connections WHERE ? LIMIT 1',
+//     { isGet: 0 },
+//     callback
+//   )
+// }
+
+// exports.test = (data, callback) => {
+//   connection.query(
+//     'insert ignore into users (vID, firstName, lastName, photo, sex, canWrite, canGetAudio, lastSeen, bdate, city, country, followers) values ?',
+//     [data],
+//     callback
+//   )
+// }
+
+// exports.addConnections = (data, callback) => {
+//   connection.query(
+//     'insert ignore into connections (type, value, userID) values ?',
+//     [data],
+//     callback
+//   )
+// }
